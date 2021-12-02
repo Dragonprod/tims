2f0978c6a70d2753683b67a8f55fd46a6f29bffb
Dragonprod <artyom2002-98@mail.ru>
2021-12-02 20:21:38 +0300

    add theme and status tags

--- frontend/src/components/StatusProjectTag/StatusProjectTag.js
+++ frontend/src/components/StatusProjectTag/StatusProjectTag.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import styles from './Tag.module.css';
+import styles from './StatusProjectTag.module.css';
 import Card from '@mui/material/Card';
 
-const statuses = {
+const projectStatuses = {
     0: "Поиск инвестора",
     1: "Уточнение деталей",
     2: "Подготовка к тестированию",
@@ -15,9 +15,9 @@ const statuses = {
     7: "Отменён",
     8: "Закрыт",
 }
-export default function Tag(props) {
+export default function StatusProjectTag(props) {
     const statusId = props.status;
-    const statusText = statuses[statusId]
+    const statusText = projectStatuses[statusId]
     return (
         <div><h1>{statusText}</h1></div>
     );
