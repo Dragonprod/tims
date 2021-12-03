e8fe787759d0db81812ab6e41d896def12f24c64
Dragonprod <artyom2002-98@mail.ru>
2021-12-03 23:30:50 +0300

    migrations

--- backend/src/database/migrate.py
+++ backend/src/database/migrate.py
@@ -180,56 +180,87 @@ async def create_startup():
     description = [
         "За последние 7 лет я создал самый передовой в мире разговорный ИИ с открытым доменом для Replika - чат-бота №1 в США с более чем 10 миллионами пользователей.",
         "Технология мониторинга может применяться как для учёта транспортных потоков, так и для адаптивного регулирования перекрёстков. Система способна определять ДТП, занятость парковочных мест, контролировать соблюдение правил дорожного движения."
+        "Система видеонаблюдения за дорожной ситуацией через беспилотники: На дорогах США, где установлено более 1 млн камер, количество аварий снизилось на 28%.",
+        "Обогреваемые остановки наземного транспорта: В Москве нет наземного городского общественного наземного электрического пассажирского автомобильного и пешеходного движения.",
+        "Программное обеспечение для анализа транспортных потоков по видео: Анализ данных по видео в реальном времени.",
+        "Автоматические двери метрополитена: Автоматические двери метро не должны закрываться.",
+        "Восстановление автобусных путей между станциями метрополитена: В Москве все станции метро соединены между собой с помощью автобусов и трамваев.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     ]
 
     author = 2
     categories = [[1, 2, 3]]
-    sertificate = "Есть"
+    sertificate = [True, False]
 
     name = [
+        "Система видеонаблюдения за дорожной ситуацией через беспилотники",
         "Обогреваемые остановки наземного транспорта",
         "Программное обеспечение для анализа транспортных потоков по видео",
+        "Автоматические двери метрополитена",
+        "Система умных пандусов для маломобильный граждан городского транспорта",
+        "Оплата проезда в метрополитене с помощью лица",
+        "Анализирующие дорожную ситуацию скоростные знаки",
+        "Радар для контроля скорости электросамокатов",
+        "Программное обеспечение для анализа уровня воды в период сильных дождей",
+        "Система анализа движения общественного городского транспорта",
+        "Система беспилотного управления поездами московского метрополитена",
+        "Система беспилотного управления автобусами обшественного транспорта",
+        "Беспилотная система доставки почты",
+        "Восстановление автобусных путей между станциями метрополитена",
+        "Единая система транспортной навигации в Москве",
+        "Система определения безбилетников в салоне по манере поведения через видеокамеры",
+        "Программное обеспечение для анализа забытых вещей в метрополитене",
+        "Программное обеспечение для анализа технического состояния общественного транспорта",
+        "Система для опредления изношенности колёсных пар вагонов метрополитена",
+        "Система анализа загруженности станиций метрополитена",
+        "Система определения влияния погодных условий на состояние поездов МЦК",
+        "Система определения влияния погодных условий на состояние поездов МЦД",
+        "Система анализа физического состояния водителей автобусов московского транспорта",
+        "Система анализа физического состояния машинистов поездов метрополитена",
+        "Система анализа уровня давления в колесах автобусов",
+        "Программное обеспечение для опредление изношенности рельс",
+        "Система прогнозирования аварийных ситуаций на дороге",
+        "Система анти-сон для водителей городского транспорта",
+        "Программное обеспечение для опредление паданения напряжения на линиях электропередач трамваем",
+        "Система экстренного торможения при аварийных ситуацией",
+        "Уличные роботы уборщики",
+        "Система плавного торможения для безопастно вождения"
     ]
 
     statuses = [[1], [1], [1], [5], [4], [2], [3], [4], [2], [4]]
     company_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     async with aiohttp.ClientSession() as session:
-        for i in range(30):
+        for i in range(40):
             await session.post("http://localhost:8080/api/v1/startup/create", json={
                 "description": random.choice(description),
                 "name": random.choice(name),
-                "author": 2,
+                "author": author,
                 "statuses": random.choice(statuses),
-                "sertificate": True,
-                "usecases": "usecases",
+                "sertificate": random.choice(sertificate),
+                "usecases": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
                 "categories": categories[0],
                 "company_id": random.choice(company_ids),
-                "brief_description": "brief",
-                "product_use_cases": "product_use_cases",
-                "usability": "usability"})
+                "brief_description": "Короткое описание продукта",
+                "product_use_cases": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                "usability": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"})
 
 
 async def create_reviews():
     print("Reviews")
 
     review = [
-        "Гениальнейший проект",
-        "Лучше проекта я в свой жизни не встречал",
-        "Я в своем познании настолько преисполнился, что я как будто бы уже сто триллионов миллиардов лет проживаю на триллионах и триллионах таких же планет, как эта Земля, мне этот мир абсолютно"
+        "Проект успешно прошел первый этап тестирования. В результате проверок были выявлены некоторые недостатки, которые оперативно были устранены. Очень очень очень длинный текст который лежит в базе.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        "Данный продукт успешно прошел все этапы тестрования и пилотирования. Из плюсов проекта можно отметить грамотную реализацию серверной части, используя MVC архитектуру. Это поможет легко обслуживать такой проект в дальнейшем без привлечения разработчиков данного стартапа."
     ]
 
     async with aiohttp.ClientSession() as session:
-        for i in range(30):
-            await session.post("http://localhost:8080/api/v1/user/review/add", json={
-                "review": random.choice(review),
-                "user_id": 2,
-                "startup_id": i + 1,
-                "mark": random.randint(0, 10)
-            })
-            await session.post("http://localhost:8080/api/v1/user/review/add", json={
-                "review": random.choice(review),
-                "user_id": 2,
-                "startup_id": i + 1,
-                "mark": random.randint(0, 10)
-            })
+        for i in range(40):
+            for j in range(random.randint(1, 15)):
+                await session.post("http://localhost:8080/api/v1/user/review/add", json={
+                    "review": random.choice(review),
+                    "user_id": 2,
+                    "startup_id": i + 1,
+                    "mark": random.randint(0, 10)
+                })
