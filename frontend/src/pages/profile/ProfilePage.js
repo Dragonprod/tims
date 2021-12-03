1d3a8b97b07d825e158bb1bb69d36a821b677e51
Dragonprod <artyom2002-98@mail.ru>
2021-12-04 00:54:44 +0300

    fix

--- frontend/src/pages/profile/ProfilePage.js
+++ frontend/src/pages/profile/ProfilePage.js
@@ -38,22 +38,7 @@ export default function ProfilePage() {
     //TODO: Get user data from redux
   }, []);
 
-  const [userId, setuserId] = useState(-1);
 
-  const [startupData, setstartupData] = useState([]);
-  const [favouriteStartupData, setfavouriteStartupData] = useState([]);
-  const [favouriteStartupDataCount, setfavouriteStartupDataCount] = useState(0);
-  const [startupReviewsData, setstartupReviewsData] = useState([]);
-
-  const [searchValue, setsearchValue] = useState(0);
-  const [rowValue, setrowValue] = useState(10);
-  const [page, setPage] = useState(1);
-  const [openDesc, setopenDesc] = useState(false);
-
-  const [solutionTabIsClicked, setsolutionTabIsClicked] = useState(true);
-  const [favouritesTabIsClicked, setfavouritesTabIsClicked] = useState(false);
-
-  const [startupForRender, setstartupForRender] = useState([]);
 
   const handleSolutionTabIsClicked = () => {
     setsolutionTabIsClicked(true);
@@ -65,114 +50,21 @@ export default function ProfilePage() {
     setfavouritesTabIsClicked(true);
   };
 
-  const handleMoreInfo = startup => {
-    setopenDesc(!openDesc);
-    setstartupForRender(startup);
-  };
-
-  const handleMoreInfoClose = () => {
-    setopenDesc(!openDesc);
-  };
-
-  const incrementCounter = () => {
-    setfavouriteStartupDataCount(state => state + 1);
-  };
-
-  const decrementCounter = () => {
-    setfavouriteStartupDataCount(state => state - 1);
-  };
-
-  const handleChange = async event => {
-    setsearchValue(event.target.value);
-    switch (event.target.value) {
-      case 0:
-        {
-          const startupsResponse = await API.get(
-            '/startup?sort_date=DESC&offset=0&limit=2000'
-          );
-          setstartupData(startupsResponse.data.startups);
-        }
-        break;
-      case 1:
-        {
-          const startupsResponse = await API.get(
-            '/startup?sort_date=ASC&offset=0&limit=2000'
-          );
-          setstartupData(startupsResponse.data.startups);
-        }
-        break;
-      case 2:
-        {
-          const startupsResponse = await API.get(
-            '/startup?sort_mark=DESCC&offset=0&limit=2000'
-          );
-          setstartupData(startupsResponse.data.startups);
-        }
-        break;
-      case 3:
-        {
-          const startupsResponse = await API.get(
-            '/startup?sort_mark=ASC&offset=0&limit=2000'
-          );
-          setstartupData(startupsResponse.data.startups);
-        }
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  const handleChangeRowValue = event => {
-    setrowValue(event.target.value);
-  };
-
-  const handlePageChange = (event, value) => {
-    setPage(value);
-  };
-
-  const addProjectToFavourites = startup => {
-    setfavouriteStartupData(favouriteStartupData => [
-      ...favouriteStartupData,
-      startup,
-    ]);
-  };
-
-  const deleteProjectToFavourites = startup => {
-    setfavouriteStartupData(
-      favouriteStartupData.filter(item => item.id !== startup.id)
-    );
-  };
 
   useEffect(() => {
-    const getStartupsData = async () => {
-      const startupsResponse = await API.get(
-        '/startup?sort_mark=DESC&offset=0&limit=2000'
-      );
-      setstartupData(startupsResponse.data.startups);
-
-      const userIdStorage = await localforage.getItem('user_id');
-      setuserId(userIdStorage);
-
-      const favouriteStartupsResponse = await API.get(
-        `/user/favorites/${userIdStorage}`
-      );
-      setfavouriteStartupData(favouriteStartupsResponse.data.favorites_startup);
-      setfavouriteStartupDataCount(
-        favouriteStartupsResponse.data.favorites_startup.length
-      );
-
-      const startupsReviewsResponse = await API.get(`/reviews`);
-      setstartupReviewsData(startupsReviewsResponse.data.reviews);
-    };
-    getStartupsData();
   }, []);
 
   return (
     <div className={styles.mainGrid}>
       <Header />
+      <div
+        className={`${styles.boldHeader} ${styles.solutionsHeader} ${solutionTabIsClicked === true ? styles.solutionsHeaderActive : ''
+          }`}
+        onClick={handleSolutionTabIsClicked}>
+
+      </div>
       <div className={styles.headerContainer}>
-        <h2 className={`${styles.boldHeader} ${styles.profileHeader}`}>
+        <h2 className={`${styles.boldHeader} ${styles.profileHeader} ${profileActive === true ? style.a : ''}`} >
           Профиль
         </h2>
         <h2 className={`${styles.boldHeader} ${styles.myStartupsHeader}`}>
