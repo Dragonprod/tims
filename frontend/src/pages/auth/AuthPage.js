3e85ad701b96285a62377d762bdc0fad92b295cb
maxcore25 <mkorobov2002@mail.ru>
2021-12-03 05:55:30 +0300

    Auth Page Form added

--- frontend/src/pages/auth/AuthPage.js
+++ frontend/src/pages/auth/AuthPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import HeaderBase from '../../components/HeaderBase/HeaderBase';
 import styles from './AuthPage.module.css';
+import { connect } from 'react-redux';
+import { setFormData } from '../../store/dataStorage/actions';
+import SlideBase from '../../assets/images/slide_base.png';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-export default function AuthPage() {
+function AuthPage() {
   return (
     <div className={styles.mainGrid}>
       <HeaderBase />
@@ -14,10 +19,52 @@ export default function AuthPage() {
       </div>
       <div className={styles.authFormContainer}>
         <div className={styles.authFormBlock}>
-          <h2>Вход</h2>
-          <h2>Вход</h2>
+          <div className={styles.authFormHeaderContainer}>
+            <h2 className={`${styles.boldHeader} ${styles.loginHeader}`}>
+              Вход
+            </h2>
+            <h2 className={`${styles.boldHeader} ${styles.signupHeader}`}>
+              Регистрация
+            </h2>
+          </div>
+          <p className={styles.authFormDescription}>
+            Авторизируйтесь, чтобы иметь возможность просматривать проекты и
+            добавлять свои
+          </p>
+          <form className={styles.authForm}>
+            <div className={styles.authFormInputDiv}>
+              <TextField
+                className={styles.authFormEmail}
+                label='E-mail'
+                variant='outlined'
+              />
+            </div>
+            <div className={styles.authFormInputDiv}>
+              <TextField
+                className={styles.authFormPassword}
+                type='password'
+                label='Пароль'
+                variant='outlined'
+              />
+            </div>
+            <p>Забыли пароль?</p>
+            <Button className={styles.muiSearchButton} variant='contained'>
+              Искать
+            </Button>
+            <p>или</p>
+            <Button className={styles.muiSearchButton} variant='outlined'>
+              Войти с помощью Google
+            </Button>
+          </form>
+          Возникли проблемы с авторизацией? Обратитесь в службу поддержки
         </div>
       </div>
     </div>
   );
 }
+
+const mapDispatchToProps = {
+  setFormData,
+};
+
+export default connect(null, mapDispatchToProps)(AuthPage);
