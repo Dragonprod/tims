e1107f5e0e964459837760d651eaab04e242c1a3
maxcore25 <mkorobov2002@mail.ru>
2021-12-04 05:15:56 +0300

    New Startup Page started

--- frontend/src/pages/newstartup/NewStartupPage.js
+++ frontend/src/pages/newstartup/NewStartupPage.js
@@ -8,16 +8,112 @@ import styles from './NewStartupPage.module.css';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
 
 import Typography from '@mui/material/Typography';
 import Pagination from '@mui/material/Pagination';
 import API from '../../api/api';
 import StatusProjectTag from '../../components/StatusProjectTag/StatusProjectTag';
 import ThemeProjectTag from '../../components/ThemeProjectTag/ThemeProjectTag';
+import Button from '@mui/material/Button';
 
 export default function NewStartupPage() {
-
   return (
-    <h1>New Startup Page</h1>
+    <div className={styles.mainGrid}>
+      <Header />
+      <div className={styles.headerContainer}>
+        <h2 className={styles.requestFormHeader}>Форма заявки</h2>
+        <span className={styles.upperHeaderSteps}>шаг 1 из 3 </span>
+      </div>
+      <div className={styles.requestFormContainer}>
+        <div className={styles.upperHeaderTabs}>
+          <h3 className={`${styles.boldHeader} ${styles.profileHeader}`}>
+            Описание организации
+          </h3>
+          <h3 className={`${styles.boldHeader} ${styles.myStartupsHeader}`}>
+            Описание проекта
+          </h3>
+          <h3 className={`${styles.boldHeader} ${styles.mySubscribesHeader}`}>
+            Запрос к акселератору
+          </h3>
+        </div>
+        <div className={`${styles.requestForm}`}>
+          <h3 className={`${styles.smallSectionHeader}`}>Контактное лицо</h3>
+          <div className={`${styles.firstLine}`}>
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Имя и отчество'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Фамилия'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Должность контактного лица'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Сайт'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Номер телефона'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='E-mail'
+              variant='outlined'
+            />
+          </div>
+          <h3 className={`${styles.smallSectionHeader}`}>Об организации</h3>
+          <div className={`${styles.secondLine}`}>
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Наименование команды/организации'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Юридическое лицо'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='ИНН'
+              variant='outlined'
+            />
+            <TextField
+              className={styles.input}
+              id='outlined-basic'
+              label='Сколько человек в организации'
+              variant='outlined'
+            />
+          </div>
+        </div>
+        <div className={styles.lowerStepButtons}>
+          <Button className={styles.muiCancelButton} variant='text'>
+            Отменить
+          </Button>
+          <Button className={`${styles.muiNextButton}`} variant='contained'>
+            Продолжить
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
